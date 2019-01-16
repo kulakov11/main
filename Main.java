4ed94882767a501b1e883c2326a88e1340c6a034
kulakov11 <46743521+kulakov11@users.noreply.github.com>
2019-01-16 12:16:01 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.stream.IntStream;
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to world");
+
+        System.out.println(isPalindrome("зачет"));
+
+        System.out.println(isPalindrome("незачет"));
+
+        for (int i = 0; i < 300; i++) {
+
+            if (isPrime(i))
+                System.out.println(i);
+        }
+    }
+
+    public static boolean isPalindrome(String string) {
+
+        return new StringBuilder(string).reverse().toString().equals(string);
+    }
+
+    public static boolean isPrime(final int num) {
+
+        return num > 1 && IntStream.range(2, num).noneMatch(index -> num % index == 0);
+    }
+}
